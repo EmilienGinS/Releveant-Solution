1001fc7cc61c18aa74da7044f08899e75816ec5a
User0 <user0@cfai.internal>
2026-02-06 09:25:20 +0000

    Initial Commit

---
+++ src/handle.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/evoliatis/buildup/cpu"
+	"github.com/evoliatis/buildup/disk"
+	"github.com/evoliatis/buildup/load"
+	"github.com/evoliatis/buildup/memory"
+	"github.com/evoliatis/buildup/netcard"
+	"github.com/evoliatis/buildup/proc"
+	"github.com/shirou/gopsutil/v4/process"
+)
+
+func HealthHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
+
+// Liste des processus
+func PSHandler(w http.ResponseWriter, r *http.Request) {
+	myProcs, err := proc.ReadProc("")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(myProcs)
+}
+
+// Liste des processus par user
+func PSUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := r.PathValue("user")
+	myProcs, err := proc.ReadProc(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(myProcs)
+}
+
+// Tuer un processus
+func PSKillHandler(w http.ResponseWriter, r *http.Request) {
+	pid := r.PathValue("pid")
+	
+	myProcs, err := process.Processes()
+	w.Header().Set("Content-Type", "application/json")
+
+	for _, proc := range myProcs {
+		if strconv.Itoa(int(proc.Pid)) == pid {
+			err := proc.Kill()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			json.NewEncoder(w).Encode("Process killed")
+			return
+		} else {
+			json.NewEncoder(w).Encode("No process found")
+			return
+		}
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func NetHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := netcard.ReadNetwork("")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(out)
+}
+
+func NetNameHandler(w http.ResponseWriter, r *http.Request) {
+	card := r.PathValue("card")
+	out, err := netcard.ReadNetwork(card)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(out)
+}
+
+func MemHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := memory.ReadMemory()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(out)
+}
+
+func DiskHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := disk.ReadDisk()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(out)
+}
+
+func LoadHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := load.ReadLoad()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(out)
+}
+
+func CPUHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := cpu.ReadCPU()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(out)
+}
